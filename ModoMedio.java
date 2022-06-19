e3cf6cc9745c5bf5179b1557e09621de17d30835
Cabra98 <gian.dallari@mi.unc.edu.ar>
2022-06-18 21:31:01 -0300

    avance

--- ModoMedio.java
+++ ModoMedio.java
@@ -1,30 +1,50 @@
-
-import java.util.Random;
-
 public class ModoMedio implements ModoDisparo{
 
     public ModoMedio(){}
 
     public void disparo(Tablero tablero, Jugador jugador){
 
+        int[][] tabla = tablero.getTabla();
+
         int flag = 0;
 
         while(flag == 0){
 
-            int y = (int) (Math.random()*10);
-            int x = (int) (Math.random()*10);
+            int i = (int) (Math.random()*10);
+            int j = (int) (Math.random()*10);
 
-            if(tablero.contenido(y,x) == 2 || tablero.contenido(y,x)== 0  || tablero.contenido(y,x) == 4){
-                tablero.recibirDisparo(y,x);
+            if(tabla[i][j] == 2 || tabla[i][j] == 0  || tabla[i][j] == 4){
+                tablero.recibirDisparo(i,j);
                 flag = 1;
 
-                if(tablero.contenido(y,x) == 3){
-                    jugador.setYIni(y);
-                    jugador.setXIni(x);
-                    jugador.setModoDisparo(new DisparoCostados());
+                if(tabla[i][j] == 2){
+                    jugador.setXIni(j);
+                    jugador.setYIni(i);
+                    if(tabla[i-1][j]!=1 && tabla[i-1][j]!=3 && i>0){
+                        jugador.setX(j);
+                        jugador.setY(i-1);
+                        jugador.setCambio(3);
+                    }
+                    else if(tabla[i][j+1]!=1 && tabla[i][j+1]!=3 && j<9){
+                        jugador.setX(j+1);
+                        jugador.setY(i);
+                        jugador.setCambio(2);
+                    }
+                    else if(tabla[i+1][j]!=1 && tabla[i+1][j]!=3 && i<9){
+                        jugador.setX(j);
+                        jugador.setY(i+1);
+                        jugador.setCambio(1);
+                    }
+                    else if(tabla[i][j-1]!=1 && tabla[i][j-1]!=3 && j>0){
+                        jugador.setX(j-1);
+                        jugador.setY(i);
+                        jugador.setCambio(0);
+                    }
 
+                    jugador.setModoDisparo(new DisparoCostados());
                 }
             }
+
         }
         jugador.setTurno(false);
     }
